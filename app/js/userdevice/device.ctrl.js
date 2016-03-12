843e09458bc7d2ecbb9887f17be439fe3f386069
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2016-03-12 03:30:01 -0500

    Positive link with SERVER

--- app/js/userdevice/device.ctrl.js
+++ app/js/userdevice/device.ctrl.js
@@ -8,6 +8,13 @@
 
      console.log('This is the Device Controller');
 
+     var vm = this;
+
+     vm.getUserDevices = function (res) {
+       DeviceServ.getUserDevices(res);
+     };
+     vm.getUserDevices({res:''});
+
    }
   ]);
 }());
--- app/js/userdevice/device.serv.js
+++ app/js/userdevice/device.serv.js
@@ -7,6 +7,15 @@
       function (SERVER, $http) {
 
         console.log('This is the Device Service');
+        var endpoint = SERVER.URL;
+
+        this.getUserDevices = function (res) {
+          return $http.get(endpoint + '/devices', SERVER.CONFIG).then(function(res){
+            console.log('These are the devices data:', res.data);
+          });
+        };
+
+
 
       }
     ]);
