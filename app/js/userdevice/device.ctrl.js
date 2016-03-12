16769c0763c75ef8e6e2f0b72de4c866cce1c9ce
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2016-03-12 05:07:10 -0500

    Rendered info to devices section

--- app/js/userdevice/device.ctrl.js
+++ app/js/userdevice/device.ctrl.js
@@ -9,9 +9,11 @@
      console.log('This is the Device Controller');
 
      var vm = this;
-
+     vm.state = {devices:[]};
      vm.getUserDevices = function (res) {
-       DeviceServ.getUserDevices(res);
+       DeviceServ.getUserDevices(res).then(function(res){
+      $scope.devices = res.data.devices;
+       });
      };
      vm.getUserDevices({res:''});
 
